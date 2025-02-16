0e6e4eb80db8ec602bc0c56d10e260471719537e
Aditya Yadav <aditya.yadav@dardoc.com>
2025-02-16 14:09:23 +0400

    fixed port

--- functions.js
+++ functions.js
@@ -12,5 +12,5 @@ connectDB();
 // Use user routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
